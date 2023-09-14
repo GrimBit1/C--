09daec4afe0cb9f298c7d10b5ac4b95f021062aa
Aditya Nandwana <adityanandwana19@gmail.com>
2023-09-14 09:22:53 +0530

    Tutorial 4 complete

---
+++ 4/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = 5;
+    char aditya = 'a';
+    char string[] = "Aditya";
+    cout << "The Value of a:" << a << ". The value of b:" << b << "\n";
+    float pi = 3.14;
+    cout << "The value of pi is " << pi;
+    return 0;
+}
+// Data Types Of C++ (built-in)
+// int
+// char
+// float
+// double
+// bool
+
+// Data Types Of C++ (derived)
+// Array
+// Pointers
+// Function
+
+// Data Types Of C++ (user defined)
+// Struct
+// Enum
+// Union
+
+// Local variable takes precedence over global variable
