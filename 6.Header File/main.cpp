2b56d6ca4836cf31a6e9fe5229eb8c8387ed8e0c
Aditya Nandwana <adityanandwana19@gmail.com>
2023-11-11 23:31:33 +0530

    Tutorial 6 Complete

---
+++ 6.Header File/main.cpp
@@ -0,0 +1,28 @@
+// There are 2 types of header files
+
+// 1. System Header Files : It Comes with Compiler
+#include <iostream>
+// 2. User Defined Header Files : Written By programmer
+#include "this.h"
+
+using namespace std;
+
+int main()
+{
+    int a = 4, b = 3;
+    cout << "Operator in C++" << endl;
+    cout << "Arithmetic Operator" << endl;
+    cout << "a + b :" << a + b << endl;
+    cout << "a - b :" << a - b << endl;
+    cout << "a * b :" << a * b << endl;
+    cout << "a / b :" << a / b << endl;
+    cout << "a % b :" << a % b << endl;
+    cout << "a++ :" << a++ << endl;
+    cout << "a-- :" << a-- << endl;
+    cout << "--a :" << --a << endl;
+    cout << "++a :" << ++a << endl;
+
+    // Assignment Operator
+    // Comparison Operator
+    // Logical Operator
+}
